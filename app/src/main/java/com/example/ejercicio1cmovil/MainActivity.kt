c58f6e37f3dcd6aa1b861ba231cd42f689ac55a6
Liliana2940 <liliana_mendoza29@comunidad.unam.mx>
2023-04-20 20:22:43 -0600

    avance

--- app/src/main/java/com/example/ejercicio1cmovil/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1cmovil/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.ejercicio1cmovil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.example.ejercicio1cmovil.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    var cuenta = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        // val nombre = binding.idName.text.toString()
+
     }
+
+
+    fun click(view: View){
+        cuenta = binding.idCuenta.text.String().toInt()
+    }
+
 }
